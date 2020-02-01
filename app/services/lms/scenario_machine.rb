9000cbe6db2b30272d24a01fe9e7ba059180df49
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2020-02-01 17:14:46 +0800

    WIP

--- app/services/lms/scenario_machine.rb
+++ app/services/lms/scenario_machine.rb
@@ -34,57 +34,85 @@ module Lms
       # eprn = ending principal
       # eint = ending interest
 
-      bbal = loan.amount.to_f
-      bprn = loan.amount.to_f
-      dint = bbal*loan.interest_per_day.to_f
-      cint = dint
-
-      cache = {
-        date: loan.start_date.strftime("%Y-%m-%d"),
-        bbal: bbal,
-        bprn: bprn,
-        dint: dint,
-        cint: cint,
-        tadd: 0.to_f,
-        tpay: 0.to_f,
-        pded: 0.to_f,
-        ided: 0.to_f,
-        ebal: bbal + dint,
-        eprn: bprn,
-        eint: cint,
-      }
+      cache = {}
 
       (loan.start_date.to_date..(loan.start_date.to_date + loan.term_count.days)).map{ |date| date.strftime("%Y-%m-%d") }.map.with_index do |day, i|
         events = loan.actual_events.where(date: day)
-        unless events.blank?
-          events.each do |event|
+        if i == 0
+          bbal = loan.amount.to_f
+          bprn = loan.amount.to_f
+          dint = bbal*loan.interest_per_day.to_f
+          cint = dint
+
+          cache = {
+            date: loan.start_date.strftime("%Y-%m-%d"),
+            bbal: bbal,
+            bprn: bprn,
+            dint: dint,
+            cint: cint,
+            tadd: 0.to_f,
+            tpay: 0.to_f,
+            pded: 0.to_f,
+            ided: 0.to_f,
+            ebal: bbal + dint,
+            eprn: bprn,
+            eint: cint,
+          }
+        else
+          unless events.blank?
+            events.each do |event|
+              bbal = cache[:ebal]
+              bprn = cache[:eprn]
+              dint = bbal*loan.interest_per_day.to_f
+              cint = cache[:cint] + dint
+
+              case event.name
+              when "add_money"
+                tadd = event.data[:amount]
+
+                eprn = bprn + tadd
+                eint = cint
+                ebal = bbal + dint + tadd
+              when "sub_money"
+                tpay = event.data[:amount]
+
+                if tpay >= cint
+                  ided = cint
+                  pded = tpay - cint
+                else
+                  ided = tpay
+                  pded = 0.to_f
+                end
+
+                ebal = (bbal + dint) - tpay
+                eprn = bprn - pded
+                eint = cint - ided
+              end
+
+              cache = {
+                date: day,
+                bbal: bbal,
+                bprn: bprn,
+                dint: dint,
+                cint: cint,
+                tadd: tadd.to_f,
+                tpay: tpay.to_f,
+                pded: pded.to_f,
+                ided: ided.to_f,
+                ebal: ebal,
+                eprn: eprn,
+                eint: eint,
+              }
+            end
+          else
             bbal = cache[:ebal]
             bprn = cache[:eprn]
             dint = bbal*loan.interest_per_day.to_f
             cint = cache[:cint] + dint
-
-            case event.name
-            when "add_money"
-              tadd = event.data[:amount]
-
-              eprn = bprn + tadd
-              eint = cint
-              ebal = bbal + dint + tadd
-            when "sub_money"
-              tpay = event.data[:amount]
-
-              if tpay >= cint
-                ided = cint
-                pded = tpay - cint
-              else
-                ided = tpay
-                pded = 0.to_f
-              end
-
-              ebal = (bbal + dint) - tpay
-              eprn = bprn - pded
-              eint = cint - ided
-            end
+            eint = cint
+            ebal = bbal + dint
+            eprn = bprn
+            eint = cint
 
             cache = {
               date: day,
@@ -92,38 +120,14 @@ module Lms
               bprn: bprn,
               dint: dint,
               cint: cint,
-              tadd: tadd.to_f,
-              tpay: tpay.to_f,
-              pded: pded.to_f,
-              ided: ided.to_f,
+              tpay: 0.to_f,
+              pded: 0.to_f,
+              ided: 0.to_f,
               ebal: ebal,
               eprn: eprn,
               eint: eint,
             }
           end
-        else
-          bbal = cache[:ebal]
-          bprn = cache[:eprn]
-          dint = bbal*loan.interest_per_day.to_f
-          cint = cache[:cint] + dint
-          eint = cint
-          ebal = bbal + dint
-          eprn = bprn
-          eint = cint
-
-          cache = {
-            date: day,
-            bbal: bbal,
-            bprn: bprn,
-            dint: dint,
-            cint: cint,
-            tpay: 0.to_f,
-            pded: 0.to_f,
-            ided: 0.to_f,
-            ebal: ebal,
-            eprn: eprn,
-            eint: eint,
-          }
         end
 
         cache
