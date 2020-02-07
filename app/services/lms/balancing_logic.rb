d1f39bb60204f7941c1c5b3327f49295b4dbe29f
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2020-02-07 17:13:23 +0800

    WIP

--- app/services/lms/balancing_logic.rb
+++ app/services/lms/balancing_logic.rb
@@ -2,11 +2,11 @@ module Lms
   class BalancingLogic
     attr_accessor :base_payments, :date_of_balance, :amortization_logic, :last_event_date
 
-    def initialize(amortization_logic, base_payments, date_of_balance)
+    def initialize(amortization_logic, base_payments, date_of_balance, last_event_date)
       @amortization_logic = amortization_logic
       @date_of_balance = date_of_balance
       @base_payments = base_payments
-      @current_date = current_date
+      @last_event_date = last_event_date
     end
 
     def execute
@@ -34,32 +34,31 @@ module Lms
     end
 
     def balance_after_early
-      loop.inject([]) do
+      loop.inject([]) do |adjustment_events|
         table = amortization_logic.execute
-        break if table[date_of_balance][:zzz_bal].round(2) == 0
+        return adjustment_events if table[date_of_balance][:zzz_bal].round(2) == 0
 
         table.each do |date, row|
           if (base_payments.keys.include? date) && (row[:zzz_bal].round(2) < 0)
-            adjustment_events << { date: row[:date], amount: row[:zzz_bal]*-1 }
+            adjustment_events << { date: date, amount: row[:zzz_bal]*-1 }
             amortization_logic.add_event(adjustment_events.last)
             break
           end
         end
-      end
 
-      adjustment_events
+        adjustment_events
+      end
     end
 
     def balance_after_late
       loop.inject([]) do |adjustment_events|
         table = amortization_logic.execute
-        break if table[date_of_balance][:zzz_bal].round(2) == 0
+        return adjustment_events if table[date_of_balance][:zzz_bal].round(2) == 0
 
-        adjustment_events << { date: date_to_manipulate, amount: -1*row_of_balance[:zzz_bal] }
+        adjustment_events << { date: last_event_date, amount: -1*table[date_of_balance][:zzz_bal] }
         amortization_logic.add_event(adjustment_events.last)
+        adjustment_events
       end
-
-      adjustment_events
     end
   end
 end
