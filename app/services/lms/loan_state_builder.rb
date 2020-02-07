4dff3dfc8d24b7a6116c0f2867ac98e1118bd4e4
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2020-02-08 00:01:08 +0800

    WIP

--- app/services/lms/loan_state_builder.rb
+++ app/services/lms/loan_state_builder.rb
@@ -6,14 +6,31 @@ module Lms
       @loan = loan
     end
 
+    def execute
+      actual_txns = transform_transactions(loan.actual_transactions)
+      expected_txns = transform_transactions(unrealized_expected_transactions)
+      txns = actual_txns + expected_txns
+
+      logic = Lms::AmortizationLogic.new(loan.amount, daily_interest_map, txns)
+      logic.execute
+    end
+
     private
 
-    def unrealized_expected_payments
-      loan.expected_transactions.where("date > ?", loan.events.last.date)
+    def unrealized_expected_transactions
+      loan.expected_transactions.
+        where("date > ?", loan.actual_transactions.last.date)
     end
 
     def daily_interest_map
-      DailyInterestMapper
+      mapper = DailyInterestMapper.new(loan.start_date, loan.interest, loan.period, loan.period_count)
+      mapper.execute
+    end
+
+    def transform_transactions(txns)
+      txns.map do |txn|
+        { date: txn.strftime("%Y-%m-%d"), amount: -1*txn.amount }
+      end
     end
   end
 end
