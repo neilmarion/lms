6f315dedd73fe85501335636777d4e205ab412ce
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2020-02-08 10:45:54 +0800

    WIP

--- spec/services/lms/loan_state_builder_spec.rb
+++ spec/services/lms/loan_state_builder_spec.rb
@@ -87,6 +87,7 @@ module Lms
         })
       end
 
+      # NOTE: balance was 101,502 but now it's only 101,210
       it "builds the loan state that will balance on the balance date" do
         service = described_class.new(loan, current_date)
         result = service.execute
