0d65068401847f0d51a2703ff8644668aa104ee3
Neil Marion dela Cruz <nmfdelacruz@gmail.com>
2020-02-09 09:15:37 +0800

    WIP

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -35,13 +35,31 @@ module Lms
     describe "scenarios" do
       context "when loan is paid on time" do
         specify do
-          allow(Date).to receive(:today).and_return("2020-03-01".to_date)
+          current_date = "2020-03-01"
+          allow(Date).to receive(:today).and_return(current_date.to_date)
           expect(loan.remaining_balance).to eq 101502.48756218905
           expect(loan.remaining_interest).to eq 1502.487562189055
           expect(loan.remaining_principal).to eq 100000.00
           expect(loan.paid_balance).to eq 0.0
           expect(loan.paid_interest).to eq 0.0
           expect(loan.paid_principal).to eq 0.0
+          expect(loan.status).to eq Loan::ONTIME
+
+          # Customer pays on time
+          current_date = "2020-04-01"
+          allow(Date).to receive(:today).and_return(current_date.to_date)
+          actual_transaction = loan.actual_transactions.create({
+            amount: -1*loan.expected_payment_per_period,
+            created_at: current_date,
+            updated_at: current_date,
+          })
+          binding.pry
+          expect(loan.remaining_balance).to eq 50751.243781094556
+          expect(loan.remaining_interest).to eq 502.4875621890551
+          expect(loan.remaining_principal).to eq 50248.7562189055
+          expect(loan.paid_balance).to eq -50751.2437810945
+          expect(loan.paid_interest).to eq -1000.00
+          expect(loan.paid_principal).to eq -49751.2437810945
           expect(loan.status).to eq Loan::ONTIME
         end
       end
